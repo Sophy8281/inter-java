2f27f9b165e9ed5a77f3a95c183c1070ce8696ee
sophy8281 <wanjikusophia3@gmail.com>
2022-02-24 16:24:53 +0300

    second commit

--- javapractice/src/main/java/com/example/javapractice/Car.java
+++ javapractice/src/main/java/com/example/javapractice/Car.java
@@ -2,22 +2,33 @@ package com.example.javapractice;
 
 public class Car {
 
-    public String color = "Black";
-    private int mNumberOfSeats = 5;
+    // public String color = "Black";
 
+    // Static allow accessing the color variable directly from the class without having to create an object
+    public static String color = "Black";
+    private int mNumberOfSeats = 5;
+    private String mInterior;
+
+    // Default Constructor
     public Car() {
-        // Constructor
+
+        mInterior = "Leather";
     }
 
-    public  Car(String chosenColor) {
+    public  Car(String chosenColor, String chosenInterior) {
         color = chosenColor;
+        mInterior = chosenInterior;
     }
 
     public int getmNumberOfSeats() {
-
-
         return mNumberOfSeats;
     }
 
+    public String getmInterior() {
+        return mInterior;
+    }
 
+    public void drive(){
+        System.out.println("Car is moving...");
+    }
 }
--- javapractice/src/main/java/com/example/javapractice/CarSimulator.java
+++ javapractice/src/main/java/com/example/javapractice/CarSimulator.java
@@ -5,9 +5,26 @@ public class CarSimulator {
 
         System.out.println("Launching car simulator....");
 
-        Car myToyota = new Car("Blue");
-        System.out.println("The color of the car is " + myToyota.color);
+        Car myToyota = new Car("Blue", "Wood");
+
+        //System.out.println("The color of the car is " + myToyota.color);
+
+        // Accessing the color static variable directly from the class without having to create an object
+        System.out.println("The color of the car is " + Car.color);
 
         System.out.println("mToyota has " + myToyota.getmNumberOfSeats() + "seats");
+
+        Car myHonda = new Car();
+
+        //System.out.println(myHonda.color + "Honda with red " + myHonda.getmInterior() + " interior");
+
+        // Accessing the color static variable directly from the class without having to create an object
+        System.out.println(Car.color + "Honda with red " + myHonda.getmInterior() + " interior");
+
+        Car.color = "Pink";
+        System.out.println("myToyota's color is " + Car.color);
+        System.out.println("myHonda's color is " + Car.color);
+
+        myToyota.drive();
     }
 }
